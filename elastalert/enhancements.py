4b5407a37bb01dd530503e12dc6dc38d35d39f1f
CristiRO <margineanucristi96@gmail.com>
2021-04-14 09:26:47 +0300

    Added FileFilterEnhancement and WeekendFilterEnhancement

--- elastalert/enhancements.py
+++ elastalert/enhancements.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .util import pretty_ts
-
+from .util import elastalert_logger
+import datetime
 
 class BaseEnhancement(object):
     """ Enhancements take a match dictionary object and modify it in some way to
@@ -23,3 +24,22 @@ class TimeEnhancement(BaseEnhancement):
 class DropMatchException(Exception):
     """ ElastAlert will drop a match if this exception type is raised by an enhancement """
     pass
+
+class FileFilterEnhancement(BaseEnhancement):
+    def process(self, match):
+        fileName = match['arguments.keyword']
+        if '/' not in fileName:
+            elastalert_logger.info('Dropped down the match with name={}. Reason: it does not appear to be a file.'.format(fileName))
+            raise DropMatchException()
+
+        match['file_name'] = match['arguments.keyword']
+        match.pop('arguments.keyword', None)
+        match.pop('num_hits', None)
+        match.pop('num_matches', None)
+
+class WeekendFilterEnhancement(BaseEnhancement):
+    def process(self, match):
+        dayOfTheWeek = datetime.datetime.today().isoweekday()
+        if dayOfTheWeek == 6 or dayOfTheWeek == 7:
+            elastalert_logger.info('Dropped down the match with name={}. Reason: nobody should work in the weekend!'.format(fileName))
+            raise DropMatchException()
